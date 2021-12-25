597807b23bb2fa1fc18ef529567c9903debd7ac4
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2021-12-26 02:29:10 +0530

    Made the Header

---
+++ shopnft/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+import themeSwitchIcon from '../assets/header/theme-switch.png'
+
+const Header = () => {
+    return <div className='header'>
+        <div className='logocontainer'>
+            <img src = {punkLogo} className='punkLogo' alt=''></img>
+        </div>
+
+        <div className='searchBar'>
+            <div className='searchIconContainer'>
+                <img src={searchIcon}></img>
+            </div>
+            <input className='searchInput' placeholder='Collection, item or user...'></input>
+        </div>
+
+        <div className='headerItems'>
+            <p>Drops</p>
+            <p>MarketPlace</p>
+            <p>Create</p>
+        </div>
+
+        <div className='headerActions'>
+            <div className='themeSwitchContainer'>
+                <img src={themeSwitchIcon}></img>
+            </div>
+        </div>
+
+        <div className='loginButton'>GETIN</div>
+
+        </div>
+}
+
+export default Header
